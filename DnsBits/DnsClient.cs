38ff976f73f7b80bab69ab7952c068ad47f67f1e
Adrians Heidens <adrians.heidens@gmail.com>
2018-08-16 22:00:07 +0300

    Basic socket UDP connect and send example.

--- DnsBits/DnsClient.cs
+++ DnsBits/DnsClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace DnsBits
 {
@@ -7,7 +9,21 @@ namespace DnsBits
 
         public void Query(string domainName)
         {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
+            IPAddress[] ipAddresses = Dns.GetHostAddresses("192.168.1.202");
+            if (ipAddresses.Length != 1)
+            {
+                throw new Exception("Got unexpected IP addresses");
+            }
+
+            IPEndPoint endPoint = new IPEndPoint(ipAddresses[0], 53);
+
+            socket.Connect(endPoint);
+
+            socket.Send(new byte[] { 0x01, 0x02 });
+
+            socket.Close();
         }
 
     }
