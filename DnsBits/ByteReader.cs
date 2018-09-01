0dc0725c7525fc2080db23cb9fbd0749a71dbf96
Adrians Heidens <adrians.heidens@gmail.com>
2018-09-02 00:25:40 +0300

    Byte reader.

---
+++ DnsBits/ByteReader.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DnsBits
+{
+    /// <summary>
+    /// Read from byte array primitive data types using big-endian arrangement.
+    /// </summary>
+    public class ByteReader
+    {
+        private MemoryStream memoryStream;
+        private int bitsOffset = 0;
+        private byte bitsByte = 0;
+
+        public ByteReader(byte[] content)
+        {
+            memoryStream = new MemoryStream(content, writable: false);
+        }
+
+        /// <summary>
+        /// Get ushort value.
+        /// </summary>
+        public ushort GetUshort()
+        {
+            var bytes = new byte[2];
+            memoryStream.Read(bytes);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return BitConverter.ToUInt16(bytes);
+        }
+
+        /// <summary>
+        /// Get uint value.
+        /// </summary>
+        public uint GetUint()
+        {
+            var bytes = new byte[4];
+            memoryStream.Read(bytes);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return BitConverter.ToUInt32(bytes);
+        }
+
+        /// <summary>
+        /// Get byte value.
+        /// </summary>
+        public byte GetByte()
+        {
+            int value = memoryStream.ReadByte();
+            if (value == -1)
+            {
+                // TODO: Choose proper exception. Add this check to other methods.
+                throw new Exception("End of stream");
+            }
+            return (byte) value;
+        }
+
+        /// <summary>
+        /// Get and decode string as UTF8.
+        /// </summary>
+        /// <param name="size">Number of bytes to get.</param>
+        public string GetString(int size)
+        {
+            var bytes = new byte[size];
+            memoryStream.Read(bytes);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        /// <summary>
+        /// Get bytes.
+        /// </summary>
+        /// <param name="count">Number of bytes to get.</param>
+        public byte[] GetBytes(int count)
+        {
+            var bytes = new byte[count];
+            memoryStream.Read(bytes);
+            return bytes;
+        }
+
+        /// <summary>
+        /// Get individual bits.
+        /// </summary>
+        /// <param name="count">Number of bits to get.</param>
+        public byte GetBits(int count)
+        {
+            // TODO: Check if in bit mode for other methods.
+            if (count < 0 || count > 7)
+            {
+                throw new ArgumentOutOfRangeException("count", count, "Value must be in range [0, 7]");
+            }
+
+            if (bitsOffset == 0)
+            {
+                bitsByte = GetByte();
+            }
+
+            byte value = BitUtils.GetBits(bitsByte, bitsOffset, count);
+
+            bitsOffset += count;
+            if (bitsOffset == 8)
+            {
+                bitsOffset = 0;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Tests/ByteReaderTest.cs
@@ -0,0 +1,26 @@
+﻿using DnsBits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class ByteReaderTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            byte[] bytes = File.ReadAllBytes(Path.Combine("Files", "structdata.dat"));
+            var reader = new ByteReader(bytes);
+
+            Assert.AreEqual(35000, reader.GetUshort());
+            Assert.AreEqual(230, reader.GetByte());
+            Assert.AreEqual("spam", reader.GetString(4));
+            Assert.AreEqual(4211001100, reader.GetUint());
+            Assert.AreEqual(1, reader.GetBits(1));
+            Assert.AreEqual(2, reader.GetBits(4));
+            Assert.AreEqual(1, reader.GetBits(3));
+            Assert.AreEqual(10, reader.GetByte());
+        }
+    }
+}
