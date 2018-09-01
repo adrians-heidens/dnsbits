4825b09ae7244ea722d98e4c32fc1cc338e48e24
Adrians Heidens <adrians.heidens@gmail.com>
2018-09-01 21:57:49 +0300

    Bit utilities.

---
+++ DnsBits/BitUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DnsBits
+{
+    public static class BitUtils
+    {
+
+        /// <summary>
+        /// Get specified bits out of byte value.
+        /// </summary>
+        /// <param name="value">Value to get bits from.</param>
+        /// <param name="index">From which bit to get (0 to 7).</param>
+        /// <param name="count">How many bits to get (0 to 8 - index).</param>
+        /// <returns>Specified bits as byte value.</returns>
+        public static byte GetBits(byte value, int index, int count)
+        {
+            if (index < 0 || index > 7)
+            {
+                throw new ArgumentOutOfRangeException("index", value, "Must be in range [0, 7]");
+            }
+            if (count < 0 || count > 8 - index)
+            {
+                throw new ArgumentOutOfRangeException("count", value, "Must be in range [0, 8 - index]");
+            }
+
+            int ones = (1 << count) - 1;
+            int offset = 8 - (index + count);
+            int mask = ones << offset;
+
+            int result = (value & mask) >> offset;
+            return (byte) result;
+        }
+
+        /// <summary>
+        /// Copy specified bits from one byte value to another.
+        /// </summary>
+        /// <param name="value">Byte value to copy to.</param>
+        /// <param name="index">From which bit to copy (0 to 7).</param>
+        /// <param name="count">How many bits to copy (0 to 8 - index).</param>
+        /// <param name="source">Byte value to copy bits from.</param>
+        /// <returns></returns>
+        public static byte SetBits(byte value, int index, int count, byte source)
+        {
+            if (index < 0 || index > 7)
+            {
+                throw new ArgumentOutOfRangeException("index", value, "Must be in range [0, 7]");
+            }
+            if (count < 0 || count > 8 - index)
+            {
+                throw new ArgumentOutOfRangeException("count", value, "Must be in range [0, 8 - index]");
+            }
+
+            int iv = 7 - index;
+            int src2 = source << (iv - count + 1);
+            int m0 = (1 << count) - 1;
+            int m1 = m0 << (iv - count + 1);
+            int m1v = 255 - m1;
+            int dst2 = m1v & value;
+
+            int result = dst2 | src2;
+            return (byte) result;
+        }
+    }
+}
---
+++ Tests/BitUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿using DnsBits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BitUtilsTests
+    {
+        [TestMethod]
+        public void GetBitsTest()
+        {
+            Assert.AreEqual(0b1, BitUtils.GetBits(0b10010001, 0, 1));
+            Assert.AreEqual(0b0, BitUtils.GetBits(0b10010001, 1, 1));
+            Assert.AreEqual(0b0, BitUtils.GetBits(0b10010001, 6, 1));
+            Assert.AreEqual(0b1, BitUtils.GetBits(0b10010001, 7, 1));
+            Assert.AreEqual(0b100, BitUtils.GetBits(0b10010001, 0, 3));
+            Assert.AreEqual(0b001, BitUtils.GetBits(0b10010001, 1, 3));
+            Assert.AreEqual(0b010, BitUtils.GetBits(0b10010001, 2, 3));
+            Assert.AreEqual(0b001, BitUtils.GetBits(0b10010001, 5, 3));
+            Assert.AreEqual(0b10010001, BitUtils.GetBits(0b10010001, 0, 8));
+        }
+
+        [TestMethod]
+        public void SetBitsTest()
+        {
+            Assert.AreEqual(0b10110001, BitUtils.SetBits(0b10010001, 2, 3, 0b110));
+            Assert.AreEqual(0b10110001, BitUtils.SetBits(0b10010001, 2, 1, 0b1));
+            Assert.AreEqual(0b00010001, BitUtils.SetBits(0b10010001, 0, 1, 0b0));
+            Assert.AreEqual(0b10010000, BitUtils.SetBits(0b10010001, 7, 1, 0b0));
+            Assert.AreEqual(0b10010010, BitUtils.SetBits(0b10010001, 6, 2, 0b10));
+            Assert.AreEqual(0b11101100, BitUtils.SetBits(0b10010001, 0, 8, 0b11101100));
+            Assert.AreEqual(0b11111111, BitUtils.SetBits(0b10010001, 0, 8, 0b11111111));
+        }
+    }
+}
