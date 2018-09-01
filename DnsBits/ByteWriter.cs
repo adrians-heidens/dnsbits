aaa4d1e869119f4c949388838ff3f81ea89f07d6
Adrians Heidens <adrians.heidens@gmail.com>
2018-09-01 22:01:41 +0300

    Byte writer.

---
+++ DnsBits/ByteWriter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DnsBits
+{
+    /// <summary>
+    /// Build byte array from primitive data types using big-endian arrangement.
+    /// </summary>
+    public class ByteWriter
+    {
+        private MemoryStream memoryStream;
+        private int bitsOffset = 0;
+        private byte bitsByte = 0;
+
+        public ByteWriter()
+        {
+            memoryStream = new MemoryStream();
+        }
+        
+        /// <summary>
+        /// Add ushort value.
+        /// </summary>
+        public void AddUshort(ushort value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            memoryStream.Write(bytes);
+        }
+
+        /// <summary>
+        /// Add uint value.
+        /// </summary>
+        public void AddUint(uint value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            memoryStream.Write(bytes);
+        }
+
+        /// <summary>
+        /// Add byte value.
+        /// </summary>
+        public void AddByte(byte value)
+        {
+            memoryStream.WriteByte(value);
+        }
+
+        /// <summary>
+        /// Add string value encoded as UTF8.
+        /// </summary>
+        public void AddString(string value)
+        {
+            memoryStream.Write(Encoding.UTF8.GetBytes(value));
+        }
+
+        /// <summary>
+        /// Add byte array.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddBytes(byte[] value)
+        {
+            memoryStream.Write(value);
+        }
+
+        /// <summary>
+        /// Add individual bits.
+        /// </summary>
+        /// <param name="count">Number of bits to add.</param>
+        /// <param name="value">Actual bits to add.</param>
+        public void AddBits(int count, byte value)
+        {
+            // TODO: assertions.
+
+            if (bitsOffset == 0)
+            {
+                bitsByte = 0;
+            }
+            bitsByte = BitUtils.SetBits(bitsByte, bitsOffset, count, value);
+            bitsOffset += count;
+            
+            if (bitsOffset == 8)
+            {
+                bitsOffset = 0;
+                AddByte(bitsByte);
+            }
+        }
+
+        /// <summary>
+        /// Get built bit array.
+        /// </summary>
+        public byte[] GetValue()
+        {
+            return memoryStream.ToArray();
+        }
+
+    }
+}
---
+++ Tests/ByteWriterTest.cs
@@ -0,0 +1,33 @@
+﻿using DnsBits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class ByteWriterTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            ByteWriter builder = new ByteWriter();
+
+            builder.AddUshort(35000);
+            builder.AddByte(230);
+            builder.AddString("spam");
+            builder.AddUint(4211001100);
+
+            builder.AddBits(1, 1);
+            builder.AddBits(4, 2);
+            builder.AddBits(3, 1);
+
+            builder.AddByte(10);
+            
+            byte[] expected = File.ReadAllBytes(Path.Combine("Files", "structdata.dat"));
+            byte[] actual = builder.GetValue();
+
+            Assert.AreEqual(BitConverter.ToString(expected), BitConverter.ToString(actual));
+        }
+    }
+}
